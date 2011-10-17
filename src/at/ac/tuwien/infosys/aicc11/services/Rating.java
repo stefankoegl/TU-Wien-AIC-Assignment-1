b0c870f5d7f6cf9a195135f8b1d8d98bc9ef1d58
Stefan Derkits <stefan@derkits.at>
2011-10-17 12:12:57 +0200

    move annotations to interface

--- src/at/ac/tuwien/infosys/aicc11/services/Rating.java
+++ src/at/ac/tuwien/infosys/aicc11/services/Rating.java
@@ -1,10 +1,20 @@
 package at.ac.tuwien.infosys.aicc11.services;
 
 //import at.ac.tuwien.infosys.aicc11.Customer;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
 import at.ac.tuwien.infosys.aicc11.Ratings;
 
+@Path("/rating/")
+@Produces("application/json")
 public interface Rating {
-	    	
-	Ratings getRating(long customerId);
+	
+    	@Produces("application/json")
+    	@GET
+    	@Path("{id}")
+	Ratings getRating(@PathParam("id") long customerId);
 
 }
--- src/at/ac/tuwien/infosys/aicc11/services/RatingService.java
+++ src/at/ac/tuwien/infosys/aicc11/services/RatingService.java
@@ -1,22 +1,12 @@
 package at.ac.tuwien.infosys.aicc11.services;
 
 //import at.ac.tuwien.infosys.aicc11.Customer;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.PathParam;
 
 import at.ac.tuwien.infosys.aicc11.Ratings;
 
-@Path("/rating/")
-@Produces("application/json")
 public class RatingService implements Rating {
 
-    	@Produces("application/json")
-	@Override
-	@GET
-    	@Path("{id}")
-	public Ratings getRating(@PathParam("id") long customerId) {
+	public Ratings getRating(long customerId) {
 		// TODO just some test value
 		if (customerId==0)
 		    return Ratings.A;
