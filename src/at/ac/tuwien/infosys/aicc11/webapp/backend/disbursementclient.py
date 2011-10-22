c1ea0e703bafa3231ef84ca5ee3da5edaa1c945b
Stefan Derkits <stefan@derkits.at>
2011-10-22 20:11:05 +0200

    more work in the clientbackend

--- src/at/ac/tuwien/infosys/aicc11/webapp/backend/disbursementclient.py
+++ src/at/ac/tuwien/infosys/aicc11/webapp/backend/disbursementclient.py
@@ -5,14 +5,32 @@ from suds.client import Client
 registryUrl = "http://stefan.derkits.at/files/registry_service.wsdl" #change to be served from JavaServer or from Local Django Server
 client = Client(registryUrl)
 
-def startDisbursement(preference):
+def startDisbursement(preference, money, customer):
     #TODO: create correct disbursement preference
-    pref = client.factory.create("cheque")
-    pref.name = "Foo Bar"
-    #TODO: do something correct with serviceUrl (atm no wsdl is accessible there)
-    serviceUrl = getDisbursementServiceUrl(pref)
-    print serviceUrl
+    s_pref = client.factory.create("cheque")
+    s_pref.name = "Foo Bar"
+    serviceUrl = getDisbursementServiceUrl(s_pref)
+    client2 = Client(serviceUrl)
+    #TODO: create correct customer & money
+    s_customer = client2.factory.create("customer")
+    s_money = client2.factory.create("money")
+    address = client2.factory.create("address")
+    address.city = "Vienna"
+    address.countryCode = "AT"
+    address.door = 10
+    address.house = 23
+    address.street = "Hauptstrasse"
+    address.postalCode = 1010
+    s_customer.customerId = 1
+    s_customer.firstName = "Stefan"
+    s_customer.lastName = "Derkits"
+    s_customer.middleName = "X."
+    s_customer.address = address
+    s_money.amount = 10000
+    s_money.currencyCode = "EUR"
+    result = client2.service.start_money_transfer_process(s_pref,s_money,s_customer)
+    print result
     
 def getDisbursementServiceUrl(disbursementPreference):
     result = client.service.query(disbursementPreference)
-    return result.location
+    return result.location + "?wsdl"
