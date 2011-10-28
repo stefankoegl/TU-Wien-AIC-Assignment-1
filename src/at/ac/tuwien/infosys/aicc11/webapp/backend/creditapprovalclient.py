87921f64139cea34410df72a37f44a8deb0aa180
Stefan Derkits <stefan@derkits.at>
2011-10-28 15:19:26 +0200

    Client Backend
    -) remove test values from disbursementclient
    -) implement creditapprovalclient

--- src/at/ac/tuwien/infosys/aicc11/webapp/backend/creditapprovalclient.py
+++ src/at/ac/tuwien/infosys/aicc11/webapp/backend/creditapprovalclient.py
@@ -1,12 +1,13 @@
 from suds.client import Client
 from webapp import settings
 
+import disbursementclient
+
 customer_client = Client(settings.WSDLS.get("customers"))
 shipping_client = Client(settings.WSDLS.get("shipping"))
 contract_client = Client(settings.WSDLS.get("contracts"))
 
 #TODO: better error handling (if no result, throw an exception??)
-#TODO: maybe fill out Rating in Customers already?
 
 def getCustomerByID(customerId):
     return customer_client.service.getCustomerByID(customerId)
@@ -14,10 +15,25 @@ def getCustomerByID(customerId):
 def getCustomerByName(name):
     return customer_client.service.getCustomerByName(name)
 
+def placeCreditRequest(request):
+    return contract_client.service.placeCreditRequest(request)
+
+def updateCreditRequest(request):
+    return contract_client.service.updateCreditRequest(request)
+
 def acceptOffer(offer):
-    #TODO: also call shipping & disbursement
+    #TODO: also call shipping
+    #TODO: do things in parallel
+    request = offer.credit_request
+    disbursementclient.startDisbursement(request.money, request.customer)
     return contract_client.service.acceptOffer(offer)
     
 def declineOffer(offer):
     return contract_client.service.declineOffer(offer)
     
+#wrapping of Factory methods to make things easier
+def createCreditRequest():
+    return contract_client.factory.create("credit_request")
+
+def createMoney():
+    return contract_client.factory.create("money")
--- src/at/ac/tuwien/infosys/aicc11/webapp/backend/disbursementclient.py
+++ src/at/ac/tuwien/infosys/aicc11/webapp/backend/disbursementclient.py
@@ -5,31 +5,11 @@ from suds.client import Client
 registryUrl = "http://stefan.derkits.at/files/registry_service.wsdl" #change to be served from JavaServer or from Local Django Server
 client = Client(registryUrl)
 
-def startDisbursement(preference, money, customer):
-    #TODO: create correct disbursement preference
-    s_pref = client.factory.create("cheque")
-    s_pref.name = "Foo Bar"
-    serviceUrl = getDisbursementServiceUrl(s_pref)
+def startDisbursement(money, customer):
+    serviceUrl = getDisbursementServiceUrl(customer.disbursement_preference)
     client2 = Client(serviceUrl)
-    #TODO: create correct customer & money
-    s_customer = client2.factory.create("customer")
-    s_money = client2.factory.create("money")
-    address = client2.factory.create("address")
-    address.city = "Vienna"
-    address.countryCode = "AT"
-    address.door = 10
-    address.house = 23
-    address.street = "Hauptstrasse"
-    address.postalCode = 1010
-    s_customer.customerId = 1
-    s_customer.firstName = "Stefan"
-    s_customer.lastName = "Derkits"
-    s_customer.middleName = "X."
-    s_customer.address = address
-    s_money.amount = 10000
-    s_money.currencyCode = "EUR"
-    result = client2.service.start_money_transfer_process(s_pref,s_money,s_customer)
-    print result
+    result = client2.service.start_money_transfer_process(customer.disbursement_preference,money,customer)
+    return result
     
 def getDisbursementServiceUrl(disbursementPreference):
     result = client.service.query(disbursementPreference)
